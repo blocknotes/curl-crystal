e34e079212a0c6d11d67b3c45ff20afc6ddc3d20
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-01-06 23:07:20 +0100

    Add get contenttype example

---
+++ examples/easy_get_contenttype.cr
@@ -0,0 +1,25 @@
+require "../curl-crystal"
+
+URL = "https://www.google.com"
+
+EMPTY_WRITE_FUNCTION = ->( contents : Void*, size : LibC::SizeT, nmemb : LibC::SizeT, userp : Void* ) {size * nmemb}
+
+# CURL init
+curl = LibCurl.curl_easy_init
+if curl
+  # CURL set options
+  LibCurl.curl_easy_setopt curl, LibCurl::CURLoption::CURLOPT_URL, URL
+  LibCurl.curl_easy_setopt curl, LibCurl::CURLoption::CURLOPT_FOLLOWLOCATION, 1
+  LibCurl.curl_easy_setopt curl, LibCurl::CURLoption::CURLOPT_WRITEFUNCTION, EMPTY_WRITE_FUNCTION
+
+  # CURL perform
+  if LibCurl.curl_easy_perform( curl ) == LibCurl::CURLcode::CURLE_OK
+    content_type = uninitialized LibC::Char*
+    if LibCurl.curl_easy_getinfo( curl, LibCurl::CURLINFO::CURLINFO_CONTENT_TYPE, pointerof( content_type ) ) == LibCurl::CURLcode::CURLE_OK
+      puts String.new( content_type )
+    end
+  end
+
+  # CURL cleanup
+  LibCurl.curl_easy_cleanup curl
+end
